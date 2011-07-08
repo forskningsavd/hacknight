5ac069762a76db829e2020bce901cdc8b7ec46fd
Olle Jonsson <olle.jonsson@gmail.com>
2011-07-08 20:06:40 +0200

    Add js effect.

---
+++ js/hacknight.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#menu a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800,'easeInOutExpo');
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+        event.preventDefault();
+    });
+});
